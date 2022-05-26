5a465d66aacf6d913fa44c770d298871bb2c1f86
ZoomQuiet <zoomquiet+git@gmail.com>
2022-05-26 20:53:47 +0800

    📖 DOC:

--- dot/dotools/gen2htm4io101camp.py
+++ dot/dotools/gen2htm4io101camp.py
@@ -99,9 +99,25 @@ http://www.cssstickyfooter.com
 %(jsActions)s
 </script>
 
+<hr/>
 
+<script src="https://giscus.app/client.js"
+        data-repo="ZoomQuiet/comment"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkzNzg1ODM2MDQ="
+        data-category="Announcements"
+        data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDQxMDg2"
+        data-mapping="url"
+        data-reactions-enabled="1"
+        data-emit-metadata="1"
+        data-input-position="top"
+        data-theme="light"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
 
-<!-- DISCUS like.. -->
+<!-- DISCUS like.. 
 <script src="https://utteranc.es/client.js"
         repo="101camp/io"
         issue-term="url"
@@ -109,6 +125,8 @@ http://www.cssstickyfooter.com
         crossorigin="anonymous"
         async>
 </script>
+
+-->
 
 <hr/>
 
